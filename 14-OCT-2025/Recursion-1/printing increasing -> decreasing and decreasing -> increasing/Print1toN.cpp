5bd882421849fd80f71ae39bf29c6443e1c38e1f
Karitk Gupta <csai23031@glbitm.ac.in>
2025-10-14 19:12:53 +0530

    Update Print1toN.cpp

--- 14-OCT-2025/Recursion-1/printing increasing -> decreasing and decreasing -> increasing/Print1toN.cpp
+++ 14-OCT-2025/Recursion-1/printing increasing -> decreasing and decreasing -> increasing/Print1toN.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
-int print_numbers(int n){
-    if(n==0) return 0;
+void print_numbers(int n){
+    if(n==0) return;
     print_numbers(n-1);
     cout<<n<<" ";
 }
